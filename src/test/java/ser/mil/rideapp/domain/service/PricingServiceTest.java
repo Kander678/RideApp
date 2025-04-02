90e89ea43c2fea78b39c8b7f46c945b3a485f295
Kander678 <136169274+Kander678@users.noreply.github.com>
2025-04-02 12:43:36 +0200

    Haversine formula and adding logs to calculatePrice

--- src/test/java/ser/mil/rideapp/domain/service/PricingServiceTest.java
+++ src/test/java/ser/mil/rideapp/domain/service/PricingServiceTest.java
@@ -7,36 +7,36 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PricingServiceTest {
     private PricingService pricingService = new PricingService();
-    private static final Localization warszawa = new Localization(52, 21);
-    private static final Localization krakow = new Localization(50, 19);
-    private static final Localization poznan = new Localization(52, 16);
-    private static final double priceKrakowWarszawa = 525;
-    private static final double priceKrakowPoznan = 612;
+    private static final Localization WARSZAWA = new Localization(52, 21);
+    private static final Localization KRAKOW = new Localization(50, 19);
+    private static final Localization POZNAN = new Localization(52, 16);
+    private static final double PRICE_KRAKOW_WARSZAWA = 525;
+    private static final double PRICE_KRAKOW_POZNAN = 612;
 
     @Test
     void shouldCalculatePrice_betweenWarszawaAndKrakow() {
         //Given //When
-        double price = pricingService.calculatePrice(warszawa, krakow);
+        double price = pricingService.calculatePrice(WARSZAWA, KRAKOW);
 
         //Then
-        assertEquals(priceKrakowWarszawa, price);
+        assertEquals(PRICE_KRAKOW_WARSZAWA, price);
     }
 
     @Test
     void shouldCalculatePrice_betweenKrakowAndWarszawa() {
         //Given //When
-        double price = pricingService.calculatePrice(krakow, warszawa);
+        double price = pricingService.calculatePrice(KRAKOW, WARSZAWA);
 
         //Then
-        assertEquals(priceKrakowWarszawa, price);
+        assertEquals(PRICE_KRAKOW_WARSZAWA, price);
     }
 
     @Test
     void shouldCalculatePrice_betweenKrakowAndPoznan() {
         //Given //When
-        double price = pricingService.calculatePrice(krakow, poznan);
+        double price = pricingService.calculatePrice(KRAKOW, POZNAN);
 
         //Then
-        assertEquals(priceKrakowPoznan, price);
+        assertEquals(PRICE_KRAKOW_POZNAN, price);
     }
 }
